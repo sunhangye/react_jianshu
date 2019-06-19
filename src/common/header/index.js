5dde0aef9ca3e3b032a37d5b2ce3374ec774ce81
sunhy <sunhy@51cto.com>
2019-06-19 11:41:46 +0800

    使用immutable管理state

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { connect } from 'react-redux';
-import store from '../../store';
 import  { actionCreators } from './store';
 import {
   HeaderWrapper,
@@ -14,51 +13,47 @@ import {
   Button
 } from './style';
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.state = store.getState();
-  }
+const Header = (props) => {
+  const { focused, handleInputFocus, handleInputBlur } = props;
   
-  render() {
-    const { focused, handleInputFocus, handleInputBlur } = this.props;
-    
-    return (
-      <HeaderWrapper>
-        <Logo />
-        <Nav>
-          <NavItem className="left active">首页</NavItem>
-          <NavItem className="left">下载APP</NavItem>
-          <SearchWrapper>
-            <CSSTransition 
-              in={focused}
-              timeout={300}
-              classNames="slide"
-            >
-              <NavSearch className={ focused ? 'focused' : '' } onFocus={handleInputFocus} onBlur={handleInputBlur}></NavSearch>
-            </CSSTransition>
-            <i className={ focused ? 'focused iconfont zoom' : 'iconfont zoom' }>&#xe623;</i>
-          </SearchWrapper>
-          <Addition>
-            <Button className="writting">
-              <i className="iconfont">&#xe615;</i>
-              写文章
-            </Button>
-            <Button className="reg">注册</Button>
-          </Addition>
-          <NavItem className="right">登录</NavItem>
-          <NavItem className="right">
-            <i className="iconfont">&#xe636;</i>
-          </NavItem>
-        </Nav>
-      </HeaderWrapper>
-    )
-  }
+  return (
+    <HeaderWrapper>
+      <Logo />
+      <Nav>
+        <NavItem className="left active">首页</NavItem>
+        <NavItem className="left">下载APP</NavItem>
+        <SearchWrapper>
+          <CSSTransition 
+            in={focused}
+            timeout={300}
+            classNames="slide"
+          >
+            <NavSearch className={ focused ? 'focused' : '' } onFocus={handleInputFocus} onBlur={handleInputBlur}></NavSearch>
+          </CSSTransition>
+          <i className={ focused ? 'focused iconfont zoom' : 'iconfont zoom' }>&#xe623;</i>
+        </SearchWrapper>
+        <Addition>
+          <Button className="writting">
+            <i className="iconfont">&#xe615;</i>
+            写文章
+          </Button>
+          <Button className="reg">注册</Button>
+        </Addition>
+        <NavItem className="right">登录</NavItem>
+        <NavItem className="right">
+          <i className="iconfont">&#xe636;</i>
+        </NavItem>
+      </Nav>
+    </HeaderWrapper>
+  )
+
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
+  
   return {
-    focused: state.header.focused
+    focused: state.header.get('focused'),
   }
 }
 
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -1,21 +1,29 @@
 import * as actionTypes from './constants'
-
-const defaultState = {
+import { fromJS } from 'immutable';
+const defaultState = fromJS({
   focused: false,
-};
-
-export default (state = defaultState, action) => {
-  if (action.type === actionTypes.SEARCH_FOCUS) {
-    
-    
-    return { // 返回一个新对象
-      focused: true,
+});
+const nested = fromJS({
+  a: {
+    b: {
+      c: [3, 4, 5]
     }
   }
+});
+console.log(nested.getIn(['a', 'b', 'c']));
+
+// 不能直接修改state!
+export default (state = defaultState, action) => {
+  /**
+   * immutable对象的set方法， 会结合之前immutable对象的值
+   * 和设置的值，返回一个全新的对象
+   */
   if (action.type === actionTypes.SEARCH_FOCUS) {
-    return {
-      focused: false,
-    }
+    
+    return state.set('focused', true);
+  }
+  if (action.type === actionTypes.SEARCH_BLUR) {
+    return state.set('focused', false);
   }
   return state
 }
